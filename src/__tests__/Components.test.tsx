35eecf22dc09210bfda6a98385e85a18f6acc11a
Valleyman <croccifixiodelena237@gmail.com>
2024-05-30 19:46:31 +0700

    Fix fetching data in unit test with 'Waitfor' from testing library

--- src/__tests__/Components.test.tsx
+++ src/__tests__/Components.test.tsx
@@ -1,7 +1,8 @@
 import { describe, expect, test } from "vitest";
-import { render, screen, } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import InfiniteList from "../components/InfiniteList";
 import NewTaskForm from "../components/NewTaskForm";
+
 window.matchMedia = window.matchMedia || function () {
     return {
         matches: false,
@@ -21,12 +22,40 @@ describe("ListInfinite", () => {
         expect(screen.getByText(/Больше нет данных./i)).toBeDefined();
     });
 });
-describe("ListInfiniteFetch", () => {
-    test("renders", () => {
-        render(<InfiniteList typeParam={"all"} />);
-        expect(screen.getByText('Список задач')).toBeDefined();
+
+describe("ListInfiniteFetch(List and List Item)", () => {
+    const renderList = () => (render(<InfiniteList typeParam="all" />));
+
+    test("renders", async () => {
+        const { getAllByRole } = renderList();
+
+        await waitFor(() => {
+            const taskList = getAllByRole('list')
+            const task = getAllByRole('listitem');
+            expect(taskList).toBeDefined();
+            expect(task).toBeDefined();
+        })
+        // render(<InfiniteList typeParam={"all"} />);
+        // expect(screen.getByText('Список задач')).toBeDefined();
     });
 });
+describe("ListInfiniteFetch(Delete List Item)", () => {
+    const renderList = () => (render(<InfiniteList typeParam="all" />));
 
+    test("renders", async () => {
+        const { getAllByText, getAllByRole } = renderList();
+
+
+        await waitFor(() => {
+            const task = getAllByRole('listitem').length;
+            const deleteButon = getAllByText(/удалить/i)
+            fireEvent.click(deleteButon[0])
+
+            waitFor(() => expect(getAllByRole('listitem').length).toBe(task - 1));
+        })
+        // render(<InfiniteList typeParam={"all"} />);
+        // expect(screen.getByText('Список задач')).toBeDefined();
+    });
+});
 
 
